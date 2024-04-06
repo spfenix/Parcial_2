cf3379b9483424c857cd4a7cc9e532bf6b40ef96
SPFENIX <cgofenix@hotmail.com>
2024-04-05 20:56:32 -0500

    Mensajes de bienvenida e informacion del juego

---
+++ Adivina_numero/Adivina_numero/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hola ¿eres capaz de adivinar el número que estoy pensando?\n");
+        Console.WriteLine("Cuantos van a jugar\n");
+        Console.WriteLine("|Digita 1 para dos jugadores   |");
+        Console.WriteLine("|Digita 2 para tres jugadores  |");
+        Console.WriteLine("|Digita 3 para cuatro jugadores|");
+    }
+}
