ad7e21dcf271fd7a90d8caf5714a3d7e2721dd99
SPFENIX <cgofenix@hotmail.com>
2024-04-05 21:12:51 -0500

    Armando el  bucle

--- Adivina_numero/Adivina_numero/Program.cs
+++ Adivina_numero/Adivina_numero/Program.cs
@@ -4,10 +4,24 @@ public class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hola ¿eres capaz de adivinar el número que estoy pensando?\n");
-        Console.WriteLine("Cuantos van a jugar\n");
-        Console.WriteLine("|Digita 1 para dos jugadores   |");
-        Console.WriteLine("|Digita 2 para tres jugadores  |");
-        Console.WriteLine("|Digita 3 para cuatro jugadores|");
-    }
-}
+        do
+        {
+            bool validador;
+            int opcion, salir;
+            Console.WriteLine("Hola ¿eres capaz de adivinar el número que estoy pensando?\n");
+            Console.WriteLine("Cuantos van a jugar\n");
+            Console.WriteLine("|Digita 1 para dos jugadores   |");
+            Console.WriteLine("|Digita 2 para tres jugadores  |");
+            Console.WriteLine("|Digita 3 para cuatro jugadores|");
+            Console.WriteLine("Digita 1 par volver a jugar o 0 para terminar");
+            salir = int.Parse(Console.ReadLine());
+            if (salir == 0)
+            {
+                validador = false;
+
+
+            }
+        } while (validador = true);
+
+
+    }   
