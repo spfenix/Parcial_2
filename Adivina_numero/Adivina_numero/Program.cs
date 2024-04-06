f4d8b80e40e2fb66604c1b2ac6b3ee8e3fd89390
SPFENIX <cgofenix@hotmail.com>
2024-04-05 22:37:14 -0500

    Usando el metodo ramdon para generar el numero.

--- Adivina_numero/Adivina_numero/Program.cs
+++ Adivina_numero/Adivina_numero/Program.cs
@@ -7,24 +7,56 @@ public class Program
         bool validador;
         do
         {
-            int opcion, salir;
+            int salir;
             Console.WriteLine("Hola ¿eres capaz de adivinar el número que estoy pensando?\n");
             Console.WriteLine("Cuantos van a jugar\n");
             Console.WriteLine("|Digita 1 para dos jugadores   |");
             Console.WriteLine("|Digita 2 para tres jugadores  |");
             Console.WriteLine("|Digita 3 para cuatro jugadores|");
-            switch
-            Console.WriteLine("Digita 1 par volver a jugar o 0 para terminar");
-            salir = int.Parse(Console.ReadLine());
-            if (salir == 0)
+            int opcion;
+            opcion = int.Parse(Console.ReadLine());
+            switch (opcion)
             {
-                validador = false ;
+                case 1:
+                    int aleatorio;
+                  Random generador = new Random();
+                    aleatorio = generador.Next();
+                    do
+                    {
+                        int num;
+                        for (int i = 1; i <= 2; i++) ;
+                        Console.WriteLine("Digita el número");
+                        num = int.Parse(Console.ReadLine());
+
+                    } while (true);
+            }
+              
 
 
-            }else validador = true ;
-
-        } while (validador == true);
 
 
 
-}   }
+
+
+
+
+
+
+
+
+
+
+
+                    Console.WriteLine("Digita 1 par volver a jugar o 0 para terminar");
+                    salir = int.Parse(Console.ReadLine());
+                    if (salir == 0)
+                    {
+
+                        validador = false;
+
+                    }
+                    else validador = true;
+
+        }while (validador == true) ;
+    }   
+}
