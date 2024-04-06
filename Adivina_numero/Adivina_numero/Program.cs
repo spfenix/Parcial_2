a49183dd3ec6d47b3fdee476dfe8876488e6af2b
SPFENIX <cgofenix@hotmail.com>
2024-04-05 21:33:57 -0500

    La variable validador debe estar por fuera del bucle

--- Adivina_numero/Adivina_numero/Program.cs
+++ Adivina_numero/Adivina_numero/Program.cs
@@ -4,15 +4,16 @@ public class Program
 {
     private static void Main(string[] args)
     {
+        bool validador;
         do
         {
-            bool validador;
             int opcion, salir;
             Console.WriteLine("Hola ¿eres capaz de adivinar el número que estoy pensando?\n");
             Console.WriteLine("Cuantos van a jugar\n");
             Console.WriteLine("|Digita 1 para dos jugadores   |");
             Console.WriteLine("|Digita 2 para tres jugadores  |");
             Console.WriteLine("|Digita 3 para cuatro jugadores|");
+            switch
             Console.WriteLine("Digita 1 par volver a jugar o 0 para terminar");
             salir = int.Parse(Console.ReadLine());
             if (salir == 0)
@@ -23,6 +24,7 @@ public class Program
             }else validador = true ;
 
         } while (validador == true);
+
 
 
 }   }
